343148abbddedbfdd37995c0fb63b49aea304ea2
eliord <elior.drori@mail.huji.ac.il>
2023-11-13 10:40:09 +0200

    2023_11_13

--- post_run_extensions/mrGrad_output2table.m
+++ post_run_extensions/mrGrad_output2table.m
@@ -1,25 +1,39 @@
-function tbl = mrGrad_output2table(RG,Axis)
-% RG    mrGrad output with one ROI (left,right) and n groups
-% pc    one axis (1/2/3)
+function tbl = mrGrad_output2table(RG,Axes)
+% A table for linear mixed-effects modelling (fitlme input)
+% -------------------------------------------------------------------------
+% INPUTS:
+% 
+%   RG        mrGrad output with one or more groups and one or more ROIs
+%   Axes      one or more ROI's axes [1-3]
+% -------------------------------------------------------------------------
+% OUTPUT:
+% 
+%   tbl     a table with variables {SubjectID, ClinicalGroup, Sex, Age,
+%           RoiName, Axis, Position, Response}
+% -------------------------------------------------------------------------
+% E.D.
+% -------------------------------------------------------------------------
 
 tbl = table;
 for gg = 1:size(RG,1)
     for rr = 1:size(RG,2)
         rg = RG{gg,rr};
-        nsegs = size(rg.Y{Axis},1);
-        nsubs = size(rg.Y{Axis},2);
-        for ii = 1:nsubs
-            t = table;
-            t.SubjectID = repmat(rg.subject_names(ii),nsegs,1);
-            t.ClinicalGroup = repmat({rg.group_name},nsegs,1);
-            t.Sex = repmat(rg.sex(ii),nsegs,1);
-            t.Age = repmat(rg.age(ii),nsegs,1);
-            t.RoiName = repmat({rg.ROI_label},nsegs,1);
-            t.Axis = repmat(Axis,nsegs,1);
-%             t.hemisphere_id = double(startsWith(t.RoiName,'Right'));
-            t.Position = (1:nsegs)';
-            t.Response = rg.Y{Axis}(:,ii);
-            tbl = [tbl;t];
+        for ax = Axes
+            nsegs = size(rg.Y{ax},1);
+            nsubs = size(rg.Y{ax},2);
+            for ii = 1:nsubs
+                t = table;
+                t.SubjectID = repmat(rg.subject_names(ii),nsegs,1);
+                t.ClinicalGroup = repmat({rg.group_name},nsegs,1);
+                t.Sex = repmat(rg.sex(ii),nsegs,1);
+                t.Age = repmat(rg.age(ii),nsegs,1);
+                t.RoiName = repmat({rg.ROI_label},nsegs,1);
+                t.Axis = repmat(ax,nsegs,1);
+    %             t.hemisphere_id = double(startsWith(t.RoiName,'Right'));
+                t.Position = (1:nsegs)';
+                t.Response = rg.Y{ax}(:,ii);
+                tbl = [tbl;t];
+            end
         end
     end
 end
