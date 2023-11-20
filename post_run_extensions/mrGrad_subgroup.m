e4abdf7c6a85ae2ea88c1bd9c43e004493f77d3c
eliord <elior.drori@mail.huji.ac.il>
2023-11-20 17:09:46 +0200

    2023_11_20

---
+++ post_run_extensions/mrGrad_subgroup.m
@@ -0,0 +1,21 @@
+function rg = mrGrad_subgroup(rg,idx,group_name)
+% given an mrGrad rg struct output (an RG cell content), and a logical
+% index, the function returns an rg struct of the indexed subgroup.
+
+
+rg.Y = cellfun(@(x) x(:,idx),rg.Y,'un',0);
+rg.Y_mean = cellfun(@(x) mean(x,2,"omitnan"),rg.Y,'un',0);
+rg.Y_std = cellfun(@(x) std(x,0,2,"omitnan"),rg.Y,'un',0);
+rg.Y_SEM = cellfun(@(x) std(x,0,2,"omitnan")/sqrt(size(x,2)),rg.Y,'un',0);
+rg.individual_data = rg.individual_data(idx);
+rg.group_name = group_name;
+
+
+field_names = fieldnames(rg);
+sub_descrips = field_names(cellfun(@(x) length(rg.(x))==length(idx),field_names));
+
+% subjects' descriptive fields
+sub_descrips(ismember(sub_descrips,{'Y','Y_mean','Y_std','Y_SEM','X','N_segments','y_lbls'})) = [];
+for jj = 1:length(sub_descrips)
+    rg.(sub_descrips{jj}) = rg.(sub_descrips{jj})(idx);
+end
