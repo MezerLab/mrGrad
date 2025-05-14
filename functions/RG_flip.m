97dec49e0e016f1a1fd02c953bc7cac38b77842b
eliord <50102348+eliord@users.noreply.github.com>
2025-05-14 14:45:05 +0300

    bug fixed in minimal putput mode

--- functions/RG_flip.m
+++ functions/RG_flip.m
@@ -16,14 +16,15 @@ for pc = pcs
     if isfield(RG,'Y_SEM')
         RG.Y_SEM{pc} = flip(RG.Y_SEM{pc});
     end
-    Nsubs = length(RG.individual_data);
-    for ii = 1:Nsubs
-        if ~isempty(RG.individual_data{ii})
-            RG.individual_data{ii}(pc).segment_inds = flip(RG.individual_data{ii}(pc).segment_inds);
-            RG.individual_data{ii}(pc).segment_inds_linear = flip(RG.individual_data{ii}(pc).segment_inds_linear);
-            RG.individual_data{ii}(pc).planes.gz = flip(RG.individual_data{ii}(pc).planes.gz);
-            RG.individual_data{ii}(pc).analysisPC_line = flip(RG.individual_data{ii}(pc).analysisPC_line, 2);
-            RG.individual_data{ii}(pc).function = flip(RG.individual_data{ii}(pc).function);
+    if isfield(RG,"individual_data")
+        for ii = 1:length(RG.individual_data)
+            if ~isempty(RG.individual_data{ii})
+                RG.individual_data{ii}(pc).segment_inds = flip(RG.individual_data{ii}(pc).segment_inds);
+                RG.individual_data{ii}(pc).segment_inds_linear = flip(RG.individual_data{ii}(pc).segment_inds_linear);
+                RG.individual_data{ii}(pc).planes.gz = flip(RG.individual_data{ii}(pc).planes.gz);
+                RG.individual_data{ii}(pc).analysisPC_line = flip(RG.individual_data{ii}(pc).analysisPC_line, 2);
+                RG.individual_data{ii}(pc).function = flip(RG.individual_data{ii}(pc).function);
+            end
         end
     end
 end
